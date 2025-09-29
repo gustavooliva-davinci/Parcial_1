d8fd76597a4cc3e9b8053977e534bfeaabf73a1f
gusoliva85 <gusoliva85@gmail.com>
2025-09-29 08:51:34 -0300

    Array de peliculas

---
+++ src/data.js
@@ -0,0 +1,8 @@
+// src/data.js
+export const peliculas = [
+    
+  { id: 1, titulo: "The Batman", año: 2022, genero: "Acción" },
+  { id: 2, titulo: "Avatar: El Camino del Agua", año: 2022, genero: "Ciencia ficción" },
+  { id: 3, titulo: "Top Gun: Maverick", año: 2022, genero: "Acción" },
+
+];
